a576ec2ee167285f6159babb4bed7b885807ed8f
gnanadeepika5 <60079729+gnanadeepika5@users.noreply.github.com>
2020-06-30 03:05:02 -0700

    React componenets & liked to code from UIelements

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom'; 
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+      <div className ="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Footer />
+      </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
